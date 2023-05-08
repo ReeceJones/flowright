0b0d3a0ed2525019f1ae937771ec06cca8fff9b5
ReeceJones <reece_jones@icloud.com>
2023-05-08 18:04:47 -0400

    Update customization structure

--- flowright/customization.py
+++ flowright/customization.py
@@ -3,15 +3,21 @@ from flowright.config import THEME
 
 _default_attribute_map = {
     'barebones': {
-        'container-attributes': {
+        'components': {
             'root': {
-                'style': 'flex-direction: column; display: flex; flex: 1 1 0'
+                'container-attributes': {
+                    'style': 'flex-direction: column; display: flex; flex: 1 1 0'
+                }
             },
             'column-container': {
-                'style': 'flex-direction: row; display: flex; justify-content: space-between; flex: 1 1 0'
+                'container-attributes': {
+                    'style': 'flex-direction: row; display: flex; justify-content: space-between; flex: 1 1 0'
+                }
             },
             'column': {
-                'style': 'flex-direction: column; display: flex'
+                'container-attributes': {
+                    'style': 'flex-direction: column; display: flex'
+                }
             }
         }
     },
@@ -20,53 +26,77 @@ _default_attribute_map = {
             '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">',
             '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>'
         ],
-        'container-attributes': {
+        'components': {
             'root': {
-                'class': 'container'
+                'container-attributes': {
+                    'class': 'container'
+                }
             },
             'column-container': {
-                'class': 'row'
+                'container-attributes': {
+                    'class': 'row'
+                }
             },
             'column': {
-                'class': 'col'
+                'container-attributes': {
+                    'class': 'col'
+                }
             },
             'table': {
-                'class': 'table table-hover'
+                'container-attributes': {
+                    'class': 'table table-hover'
+                }
             },
             'table-row': {
-                'scope': 'row'
+                'container-attributes': {
+                    'scope': 'row'
+                }
             },
             'table-column': {
-                'scope': 'col'
+                'container-attributes': {
+                    'scope': 'col'
+                }
             },
             'checkbox': {
-                'class': 'form-check'
+                'container-attributes': {
+                    'class': 'form-check'
+                },
+                'attributes': {
+                    'class': 'form-check-input'
+                }
             },
             'radio': {
-                'class': 'form-check'
-            }
-        },
-        'attributes': {
+                'container-attributes': {
+                    'class': 'form-check'
+                },
+                'attributes': {
+                    'class': 'form-check-input'
+                }
+            },
             'button': {
-                'class': 'btn btn-primary'
+                'attributes': {
+                    'class': 'btn btn-primary'
+                }
             },
             'selectbox': {
-                'class': 'form-control form-select'
+                'attributes': {
+                    'class': 'form-control form-select'
+                }
             },
             'textbox': {
-                'class': 'form-control'
+                'attributes': {
+                    'class': 'form-control'
+                }
             },
             'image': {
-                'class': 'rounded mx-auto d-block'
+                'attributes': {
+                    'class': 'rounded mx-auto d-block'
+                }
             },
             'multiselect': {
-                'class': 'form-control form-select'
-            },
-            'checkbox': {
-                'class': 'form-check-input'
-            },
-            'radio': {
-                'class': 'form-check-input'
+                'attributes': {
+                    'class': 'form-control form-select'
+                }
             }
         }
     }
@@ -81,14 +111,14 @@ def build_preload() -> str:
 
 
 def build_attributes(config_name: str) -> str:
-    attr_map = _default_attribute_map[THEME].get('attributes', {}).get(config_name)
+    attr_map = _default_attribute_map[THEME].get('components', {}).get(config_name, {}).get('attributes')
     if attr_map is None:
         return ''
     return ' '.join([f'{attr_name}="{attr_value}"' for attr_name, attr_value in attr_map.items()])
 
 
 def build_container_attributes(config_name: str) -> str:
-    attr_map = _default_attribute_map[THEME].get('container-attributes', {}).get(config_name)
+    attr_map = _default_attribute_map[THEME].get('components', {}).get(config_name, {}).get('container-attributes')
     if attr_map is None:
         return ''
     return ' '.join([f'{attr_name}="{attr_value}"' for attr_name, attr_value in attr_map.items()])
